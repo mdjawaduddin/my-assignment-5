ff0eec5601f78d629a1b8e877bfa3575e302fad9
mdjawaduddin <naweedjawad102@gmail.com>
2026-09-12 20:45:25 +0600

    banner updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+
 import Banner from "./components/Banner";
+
 import Nav from "./components/Nav";
 
 
 
+
+const techFetch = async() => {
+  const res = await fetch ('/data.json');
+ const data = await  res.json();
+ return data;
+}
+
 function App() {
+const techPromise = techFetch();
 
-
+           
+           
   return (
     <>
+     
       <Nav ></Nav>
       <div className="divider"></div>
       <Banner></Banner>
+      
       
     </>
   )
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,10 +2,11 @@ import BannerLogo from "../assets/banner-stack.png"
 
 const Banner = () => {
     return (
-        <div className="flex justify-around items-center  container mx-auto mt-24" >
-            <div className="pl-3.5" >
-                <p className="text-[60px] font-extrabold leading-none ">Build Your Ideal</p>
-                <p className="text-[60px] font-extrabold leading-none bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] text-transparent bg-clip-text">Development Stack</p>
+        <div  >
+            <span className="flex justify-around items-center  container mx-auto mt-24">
+             <div className="pl-4">
+                <p className="text-[60px] font-extrabold leading-none ">Build Your Ideal <br />
+                <span className="text-[60px] font-extrabold  bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] text-transparent bg-clip-text"> Development Stack</span> </p>
                 <p className=" text-[#475569] text-[18px] pt-6  ">
                     Explore frontend, backend, database, and tooling options,<br />
                     compare them side by side, and put together the stack that fits your <br/>
@@ -17,12 +18,22 @@ const Banner = () => {
                     <button className="btn btn-ghost border-2 border-[#E5E7EB] rounded-lg py-3 px-4">Learn More</button>
                 </div>
 
+
             </div>
             <div>
                 <img src={BannerLogo} alt="" />
-            </div>
-
+            </div>    </span>
+                <div className="mt-28 container mx-auto pl-28">
+            <p className="text-[36px] font-extrabold"> Explore the <span className="text-[36px] font-extrabold  bg-linear-to-r from-[#D81B7E] to-[#7C3AED] text-transparent bg-clip-text"> Technologies</span> </p>
+            <p className=" text-[18px] text-[#475569]">Pick one technology per category to build your ideal stack.</p>
         </div>
+            
+
+              </div>
+              
+          
+
+      
 
     );
 };
